2c05428e679ca22fa7e94a95ac1b41a6bdc52fc1
bfred-it <bfred-it@users.noreply.github.com>
2014-06-29 23:16:56 +0200

    Connect is now one-click.
    * If a textarea is focused, use it.
    * If only one textarea is present, use it.
    * If more than one textarea is present, use the first one that gains
    focus.
    * If no textareas are present, alert the user

--- scripts/background.js
+++ scripts/background.js
@@ -1,3 +1,12 @@
 $(document).ready(function () {
     GhostText.connectionHandler();
 });
+chrome.browserAction.onClicked.addListener(function () {
+    chrome.tabs.query({
+        active: true,
+        currentWindow: true
+    },
+    function(tabs){
+        chrome.tabs.sendMessage(tabs[0].id, {textarea: 'connect', id: tabs[0].id}, function(response) { });
+    });
+});
--- scripts/content.js
+++ scripts/content.js
@@ -3,11 +3,28 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
         return;
     }
 
-    sendResponse({textarea: 'connecting'});
+    var connectTextarea = function (textarea) {
+        GhostText.connectTextArea($(textarea), $('title').text(), request.id, window.location);
+    };
 
-    $("textarea").focus(function () {
-        var textArea = $(this);
-        textArea.unbind("focus");
-        GhostText.connectTextArea($(this), $('title').text(), request.id, window.location);
-    });
+    sendResponse({textarea: 'connecting'});
+    var $textareas = $('textarea');
+    var $focusedTextarea = $textareas.filter(':focus');
+    if ($focusedTextarea.length) {
+        console.log('Textarea already focused:', $focusedTextarea);
+        connectTextarea($focusedTextarea);
+    } else {
+        console.log('Textareas found:', $textareas);
+        switch  ($textareas.length) {
+            case 0: alert('No textarea elements on this page'); break;
+            case 1: connectTextarea($textareas); break;
+            default:
+                var connectAndForgetTheRest = function () {
+                    console.log('User focused:', this);
+                    connectTextarea(this);
+                    $textareas.off('.ghost-text');
+                };
+                $textareas.on('focus.ghost-text', connectAndForgetTheRest);
+        }
+    }
 });
