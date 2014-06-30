f7b5e7d88006aad4124bde30b14259409932b690
Federico Brigante <bfred-it@users.noreply.github.com>
2014-06-30 14:24:17 +0200

    Button now acts as a toggle

--- scripts/content.js
+++ scripts/content.js
@@ -1,35 +1,51 @@
-chrome.runtime.onMessage.addListener(function(request) {
-    if (!request.action || request.action !== 'button-clicked') {
-        return;
+var isConnected = false;
+var isWaitingForUser = false;
+function reactToButtonClicked (request) {
+    if (request.action && request.action == 'button-clicked') {
+        if(isWaitingForUser) {
+            $textareas.off('.ghost-text');
+        }
+        if (isConnected) {
+            closeConnection(request);
+        } else {
+            openConnection(request);
+        }
     }
-
+}
+function closeConnection (request) {
+    isConnected = false;
+    chrome.runtime.sendMessage({
+        action: 'close-connection',
+        tabId: request.tabId
+    });
+}
+function openConnection (request) {
     var connectTextarea = function (textarea) {
+        isConnected = true;
+        isWaitingForUser = false;
         GhostText.connectTextArea($(textarea), $('title').text(), request.tabId, window.location);
         window.addEventListener('beforeunload', function () {
-            chrome.runtime.sendMessage({
-                action: 'close-connection',
-                tabId: request.tabId
-            });
         });
     };
 
     var $textareas = $('textarea');
     var $focusedTextarea = $textareas.filter(':focus');
     if ($focusedTextarea.length) {
-        console.log('Textarea already focused:', $focusedTextarea);
-        connectTextarea($focusedTextarea);
-    } else {
-        console.log('Textareas found:', $textareas);
-        switch  ($textareas.length) {
-            case 0: alert('No textarea elements on this page'); break;
-            case 1: connectTextarea($textareas); break;
-            default:
-                var connectAndForgetTheRest = function () {
-                    console.log('User focused:', this);
-                    connectTextarea(this);
-                    $textareas.off('.ghost-text');
-                };
-                $textareas.on('focus.ghost-text', connectAndForgetTheRest);
-        }
+        $textareas = $focusedTextarea;
     }
-});
+    switch  ($textareas.length) {
+        case 0: alert('No textarea elements on this page'); break;
+        case 1: connectTextarea($textareas); break;
+        default:
+            isWaitingForUser = true;
+            var connectAndForgetTheRest = function () {
+                console.log('User focused:', this);
+                connectTextarea(this);
+                $textareas.off('.ghost-text');
+            };
+            $textareas.on('focus.ghost-text', connectAndForgetTheRest);
+    }
+}
+
+
+chrome.runtime.onMessage.addListener(reactToButtonClicked);
